c370c10a8eb38642fd8ccd8db27820906d909e8f
SHUBHAM KUMAR SINGH <92092771+shub1407@users.noreply.github.com>
2023-10-30 23:18:21 +0530

    Add files via upload

---
+++ mapdata.js
@@ -0,0 +1,266 @@
+var simplemaps_countrymap_mapdata={
+  main_settings: {
+   //General settings
+    width: "350", //'700' or 'responsive'
+    background_color: "#FFFFFF",
+    background_transparent: "yes",
+    border_color: "#ffffff",
+    
+    //State defaults
+    state_description: "State description",
+    state_color: "green",
+    state_hover_color: "darkorange",
+    state_url: "https://simplemaps.com",
+    border_size: 1.5,
+    all_states_inactive: "no",
+    all_states_zoomable: "yes",
+    
+    //Location defaults
+    location_description: "Location description",
+    location_color: "#FF0067",
+    location_opacity: 0.8,
+    location_hover_opacity: 1,
+    location_url: "",
+    location_size: 25,
+    location_type: "square",
+    location_image_source: "frog.png",
+    location_border_color: "#FFFFFF",
+    location_border: 2,
+    location_hover_border: 2.5,
+    all_locations_inactive: "no",
+    all_locations_hidden: "no",
+    
+    //Label defaults
+    label_color: "#d5ddec",
+    label_hover_color: "#d5ddec",
+    label_size: 22,
+    label_font: "Arial",
+    hide_labels: "no",
+    hide_eastern_labels: "no",
+   
+    //Zoom settings
+    zoom: "yes",
+    back_image: "no",
+    initial_back: "no",
+    initial_zoom: "-1",
+    initial_zoom_solo: "no",
+    region_opacity: 1,
+    region_hover_opacity: 0.6,
+    zoom_out_incrementally: "yes",
+    zoom_percentage: 0.99,
+    zoom_time: 0.5,
+    
+    //Popup settings
+    popup_color: "white",
+    popup_opacity: 0.9,
+    popup_shadow: 1,
+    popup_corners: 5,
+    popup_font: "12px/1.5 Verdana, Arial, Helvetica, sans-serif",
+    popup_nocss: "no",
+    
+    //Advanced settings
+    div: "map",
+    auto_load: "yes",
+    url_new_tab: "no",
+    images_directory: "default",
+    fade_time: 0.1,
+    link_text: "View Website",
+    popups: "detect",
+    state_image_url: "",
+    state_image_position: "",
+    location_image_url: ""
+  },
+  state_specific: {
+    "1": {
+      name: "Andaman And Nicobar",
+      url: "https://www.adotrip.com/state-detail/Andaman-nicobar",
+      description: " Port Blair"
+    },
+    "2": {
+      name: "Andhra Pradesh",
+      url: "https://www.adotrip.com/state-detail/Andhra-Pradesh",
+      description: " Amaravati"
+    },
+    "3": {
+      name: "Arunachal Pradesh",
+      url: "https://www.adotrip.com/state-detail/Arunachal-Pradesh",
+      description: " Itanagar"
+    },
+    "4": {
+      name: "Assam",
+      url: "https://www.adotrip.com/state-detail/Assam",
+      description: " Dispur"
+    },
+    "5": {
+      name: "Bihar",
+      url: "https://www.adotrip.com/state-detail/Bihar",
+      description: " Patna"
+    },
+    "6": {
+      name: "Chandigarh",
+      url: "https://www.adotrip.com/state-detail/Chandigarh",
+      description: " Chandigarh"
+    },
+    "7": {
+      name: "Chhattisgarh",
+      url: "https://www.adotrip.com/state-detail/Chhattisgarh",
+      description: " Raipur"
+    },
+    "8": {
+      name: "Dadra And Nagar Haveli",
+      url: "https://www.adotrip.com/state-detail/dadra-and-nagar-haveli",
+      description: " Silvassa"
+    },
+    "9": {
+      name: "Daman And Diu",
+      url: "https://www.adotrip.com/state-detail/daman-and-diu",
+      description: " Daman"
+    },
+    "10": {
+      name: "Delhi",
+      url: "https://www.adotrip.com/state-detail/Delhi",
+      description: " New Delhi"
+    },
+    "11": {
+      name: "Goa",
+      url: "https://www.adotrip.com/state-detail/Goa",
+      description: " Panaji"
+    },
+    "12": {
+      name: "Gujarat",
+      url: "https://www.adotrip.com/state-detail/Gujarat",
+      description: " Gandhinagar"
+    },
+    "13": {
+      name: "Haryana",
+      url: "https://www.adotrip.com/state-detail/Haryana",
+      description: " Chandigarh"
+    },
+    "14": {
+      name: "Himachal Pradesh",
+      url: "https://www.adotrip.com/state-detail/Himachal Pradesh",
+      description: " Shimla"
+    },
+    "16": {
+      name: "Jharkhand",
+      description: " Ranchi",
+      url: "https://www.adotrip.com/state-detail/Jharkhand"
+    },
+    "17": {
+      name: "Karnataka",
+      url: "https://www.adotrip.com/state-detail/Karnataka",
+      description: " Bengaluru"
+    },
+    "18": {
+      name: "Kerala",
+      url: "https://www.adotrip.com/state-detail/Kerala",
+      description: " Thiruvananthapuram"
+    },
+    "19": {
+      name: "Lakshadweep",
+      url: "https://www.adotrip.com/state-detail/Lakshadweep",
+      description: " Kavaratti"
+    },
+    "20": {
+      name: "Madhya Pradesh",
+      url: "https://www.adotrip.com/state-detail/Madhya-Pradesh",
+      description: " Bhopal"
+    },
+    "21": {
+      name: "Maharashtra",
+      url: "https://www.adotrip.com/state-detail/Maharashtra",
+      description: " Mumbai"
+    },
+    "22": {
+      name: "Manipur",
+      url: "https://www.adotrip.com/state-detail/Manipur",
+      description: " Imphal"
+    },
+    "23": {
+      name: "Meghalaya",
+      url: "https://www.adotrip.com/state-detail/Meghalaya",
+      description: " Shillong"
+    },
+    "24": {
+      name: "Mizoram",
+      url: "https://www.adotrip.com/state-detail/Mizoram",
+      description: " Aizawl"
+    },
+    "25": {
+      name: "Nagaland",
+      url: "https://www.adotrip.com/state-detail/Nagaland",
+      description: " Kohima"
+    },
+    "26": {
+      name: "Orissa",
+      url: "https://www.adotrip.com/state-detail/Orissa",
+      description: " Bhubaneswar"
+    },
+    "27": {
+      name: "Puducherry",
+      url: "https://www.adotrip.com/state-detail/Puducherry",
+      description: " Puducherry"
+    },
+    "28": {
+      name: "Punjab",
+      url: "https://www.adotrip.com/state-detail/Punjab",
+      description: " Chandigarh"
+    },
+    "29": {
+      name: "Rajasthan",
+      url: "https://www.adotrip.com/state-detail/Rajasthan",
+      description: " Jaipur"
+    },
+    "30": {
+      name: "Sikkim",
+      url: "https://www.adotrip.com/state-detail/Sikkim",
+      description: " Gangtok"
+    },
+    "31": {
+      name: "Tamil Nadu",
+      url: "https://www.adotrip.com/state-detail/Tamil-Nadu",
+      description: " Chennai"
+    },
+    "32": {
+      name: "Tripura",
+      url: "https://www.adotrip.com/state-detail/Tripura",
+      description: " Agartala"
+    },
+    "33": {
+      name: "Uttar Pradesh",
+      url: "https://www.adotrip.com/state-detail/Uttar-Pradesh",
+      description: " Lucknow"
+    },
+    "34": {
+      name: "Uttaranchal",
+      url: "https://www.adotrip.com/state-detail/Uttaranchal",
+      description: " Dehradun"
+    },
+    "35": {
+      name: "West Bengal",
+      url: "https://www.adotrip.com/state-detail/West-Bengal",
+      description: " Kolkata"
+    },
+    "36": {
+      name: "Jammu And Kashmir",
+      url: "https://www.adotrip.com/state-detail/jammu-and-kashmir",
+      description: " Srinagar (Summer)"
+    },
+    "37": {
+      name: "Telangana",
+      url: "https://www.adotrip.com/state-detail/Telangana",
+      description: " Hyderabad"
+    },
+    "38": {
+      name: "Ladakh",
+      url: "https://www.adotrip.com/state-detail/leh-ladakh",
+      description: " Leh"
+    }
+  },
+  locations: {},
+  labels: {},
+  legend: {
+    entries: []
+  },
+  regions: {}
+};
